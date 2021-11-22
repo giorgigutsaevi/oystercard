9604a4f18b822b77d933208032de704c6d76d57b
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-11-22 16:06:56 +0000

    Step #5: Enable top-up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,15 @@ describe Oystercard do
 		card = Oystercard.new
 		expect(card.balance).to eq 0
 	end
+
+	it 'responds to the #top_up method with 1 argument' do
+		expect(subject).to respond_to(:top_up).with(1).argument
+	end
+
+	it 'adds the amount to the balance of the card' do
+		card = Oystercard.new
+		card.top_up(20)
+		expect(card.balance).to eq 20
+	end
+
 end
