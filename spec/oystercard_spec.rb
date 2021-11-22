bd2792d965a191df9a8481eabe633f1f4a3b999b
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-11-22 15:54:39 +0000

    Challenge #4: card balance defaults to 0 and rspec unit-test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
+	it 'creates an instance of itself' do
+		expect(Oystercard).to respond_to(:new)
+	end
+
+	it 'The default card balance is 0' do
+		card = Oystercard.new
+		expect(card.balance).to eq 0
+	end
 end
